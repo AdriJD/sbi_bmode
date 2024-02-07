6efd4bba2e5fa46d638d81499e8526d05e8e7ffc
Alexandre Adler <AERAdler@github.com>
2024-02-07 22:31:48 +0100

    better binning function

--- test_density_estimators.py
+++ test_density_estimators.py
@@ -7,7 +7,7 @@ from torch.distributions import half_normal
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 
-def bin_spectrum(cls, lmin=2, lmax=383, binwidth=10, return_error=False):
+def bin_spectrum(cls, lmin=2, lmax_intern=200, lmax=383, binwidth=10, big_bin=True):
     """
     Bin a 1d array of power spectra using the standard binning.
 
@@ -21,26 +21,26 @@ def bin_spectrum(cls, lmin=2, lmax=383, binwidth=10, return_error=False):
         ell of the last bin for a single spectrum. Default: 383
     binwidth : scalar, optional
         Width of each bin.  Default: 10.
+    big_bin : bool, optional
+        Keep the bin after lmax_intern. Default: True
 
     Returns
     -------
-    ellb : array_like
-        Bin centers
     clsb : array_like
         Binned power spectra
-    clse : array_like
-        Error bars on each bin (if return_error is True)
     """
     ncl = len(cls)
     ns = int(ncl/(lmax+1))
     starts = np.arange(0, ncl, lmax+1)
     ell = np.arange(ncl)
-    internal_bins = np.arange(lmin, lmax + 1, binwidth)
+    internal_bins = np.arange(lmin, lmax_intern+1, binwidth)
     ibib, stst = np.meshgrid(internal_bins, starts)
     bins = (ibib+stst).flatten()
     bins = np.append(bins, [ncl])
-    #ellb = stats.binned_statistic(ell, ell, "mean", bins=bins).statistic
     clsb = stats.binned_statistic(ell, cls, "mean", bins=bins).statistic
+    if not big_bin:
+        n_int = len(internal_bins)
+        clsb = np.delete(clsb, np.s_[n_int-1::n_int])
     return clsb
 
 
@@ -51,7 +51,7 @@ nCl = (len(freqs)**2)*(2*2)*(3*nside)*nsplits
 #Per-row binning cooking
 binwidth = 10
 nspec = (len(freqs)**2)*(2*2)*nsplits #Spectra in a row
-nbins = len(np.arange(2, 3*nside, binwidth))*nspec #Bins per spectrum
+nbins = len(np.arange(2, 201, binwidth))*nspec #Bins per spectrum
 
 log_r_vector = np.log(np.load("r_vector_splits_masked.npy"))
 data_tensor = np.load("data_tensor_splits_masked.npy")
@@ -64,7 +64,7 @@ for i in range(nsamples):
     binned_data_tensor[i] = bin_spectrum(data_tensor[i])
 print(f"Binned {nsamples} samples (binwidth {binwidth}) in {time()-t1:.3f}s")
 
-density_estimators = ["mdn", "made", "maf", "maf_rqs", "nsf"]
+density_estimators = ["maf", "mdn", "made", "maf_rqs", "nsf"]
 
 #simulation parameters
 mean_params = dict()
@@ -86,6 +86,6 @@ for i, de in enumerate(density_estimators):
         binned_obs = bin_spectrum(obs)
         sample_save_de[i,j] = posterior.sample((10000,), x=binned_obs)[:,0]
 stats_de = np.percentile(sample_save_de, [2.5, 50, 97.5], axis=2)
-np.save("samples_density_estimators_binned10.npy", sample_save_de)
-np.save("stats_density_estimators_binned10.npy", stats_de)
+np.save("samples_density_estimators_binned10_lmax200.npy", sample_save_de)
+np.save("stats_density_estimators_binned10_lmax200.npy", stats_de)
 
