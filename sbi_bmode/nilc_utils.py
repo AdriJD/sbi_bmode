fixed bug in normalization for NILC, simple normalization has now moved from cmbsimulator object to the run_sbi_basic.py script, also removed 1e6 K->uK conversion inside nilc_utils.py should not be needed, but also can't hurt

sbi_bmode/nilc_utils.py
@@ -122,9 +122,11 @@ def get_nilc_maps(pyilc_path, map_tmpdir, nsplit, nside, beta_dust, temp_dust, f
         
         #load NILC maps, then remove nilc tmpdir
         cmb_nilc = hp.read_map(f'{nilc_tmpdir}/needletILCmap_component_CMB.fits')
-        dust_nilc = 10**6*hp.read_map(f'{nilc_tmpdir}/needletILCmap_component_CIB.fits')
+        #dust_nilc = 10**6*hp.read_map(f'{nilc_tmpdir}/needletILCmap_component_CIB.fits')
+        dust_nilc = hp.read_map(f'{nilc_tmpdir}/needletILCmap_component_CIB.fits')        
         if use_dbeta_map:
-            dbeta_nilc = 10**6*hp.read_map(f'{nilc_tmpdir}/needletILCmap_component_CIB_dbeta.fits')
+            #dbeta_nilc = 10**6*hp.read_map(f'{nilc_tmpdir}/needletILCmap_component_CIB_dbeta.fits')
+            dbeta_nilc = hp.read_map(f'{nilc_tmpdir}/needletILCmap_component_CIB_dbeta.fits')            
             nilc_maps[split] = np.array([cmb_nilc, dust_nilc, dbeta_nilc])
         else:
             nilc_maps[split] = np.array([cmb_nilc, dust_nilc])

sbi_bmode/sim_utils.py
@@ -34,9 +34,11 @@ class CMBSimulator():
         auto- and cross-spectra in the data vector
     odir: str
         Path to output directory
-    norm_params: 
-    score_params: 
-
+    norm_params: dict, optional
+        Parameters of fiducial model used to whiten the (multifrequency) data.
+    score_params: dict, optional
+        Parameters of fiducial model where the score is evaluted for score
+        compression.
     '''
     
     def __init__(self, specdir, data_dict, fixed_params_dict, pyilcdir=None, use_dbeta_map=False,
@@ -99,7 +101,7 @@ class CMBSimulator():
             self.isqrt_norm_cov = mat_utils.matpow(norm_cov, -0.5, return_diag=True)  
             
         self.score_params = score_params
-        if self.score_params:
+        if self.score_params and pyilcdir is None:
             
             if self.score_params and self.norm_params:
                 raise ValueError('Cannot have both norm_params and score_params')
@@ -224,9 +226,10 @@ class CMBSimulator():
                     B_maps[split, f] = 10**(-6)*hp.alm2map(alm_B, self.nside)
             map_tmpdir = nilc_utils.write_maps(B_maps, output_dir=self.odir)
             nilc_maps = nilc_utils.get_nilc_maps(self.pyilcdir, map_tmpdir, self.nsplit, self.nside, 
-                                                beta_dust, self.temp_dust, self.freq_pivot_dust, 
-                                                so_utils.sat_central_freqs, so_utils.sat_beam_fwhms,
-                                                use_dbeta_map=self.use_dbeta_map, output_dir=self.odir, remove_files=True, debug=False)
+                                                 beta_dust, self.temp_dust, self.freq_pivot_dust, 
+                                                 so_utils.sat_central_freqs, so_utils.sat_beam_fwhms,
+                                                 use_dbeta_map=self.use_dbeta_map, output_dir=self.odir,
+                                                 remove_files=True, debug=False)
             spectra = estimate_spectra_nilc(nilc_maps, self.minfo, self.ainfo)
         
         else:
@@ -235,12 +238,11 @@ class CMBSimulator():
         data = get_final_data_vector(spectra, self.bins, self.lmin, self.lmax)
 
         if self.norm_params:
-            simple = True if self.pyilcdir is not None else False
-            data = self.get_norm_data(data, simple=simple)
+            data = self.get_norm_data(data)
             
         return data
 
-    def get_norm_data(self, data, simple=False):
+    def get_norm_data(self, data):
         '''
         Subtract mean and multiply by inverse sqrt of covariance.
 
@@ -248,30 +250,23 @@ class CMBSimulator():
         ----------
         data : (ndata) array
             Input data.
-        simple: Bool, if True, subtracts mean and divides by sqrt(var) for each element
         
         Returns
         -------
         data_norm : (ndata) array
             Normalized data.
         '''
-        if simple:
-            if not hasattr(self, 'data_mean'):
-                # This is to make sure that we re-use the mean and std
-                # from the first round in sequential NPE.
-                self.data_mean = np.mean(data, axis=0)
-                self.data_std = np.std(data, axis=0)
-            data_norm = (data-self.data_mean)/self.data_std
-            return data_norm
+        
         ntri = get_ntri(self.nsplit, self.nfreq)
         tri_indices = get_tri_indices(self.nsplit, self.nfreq)
         data = likelihood_utils.get_diff(
             data.reshape(ntri, -1), self.norm_model, tri_indices)
         data = np.einsum('ijk, jk -> ik', self.isqrt_norm_cov, data)
         data_norm = data.reshape(-1)
+        
         return data_norm
 
-    def get_unnorm_data(self, data_norm, simple=False):
+    def get_unnorm_data(self, data_norm):
         '''
         Subtract mean and multiply by inverse sqrt of covariance.
 
@@ -279,17 +274,13 @@ class CMBSimulator():
         ----------
         data_norm : (ndata) array
             Nomalized data.
-        simple: Bool, if True, norm computed by subtracting mean and dividing
-                 by sqrt(var) for each element
         
         Returns
         -------
         data : (ndata) array
             Unnormalized data.
         '''
-        if simple:
-            data = data_norm * self.data_std + self.data_mean
-            return data
+
         ntri = get_ntri(self.nsplit, self.nfreq)
         tri_indices = get_tri_indices(self.nsplit, self.nfreq)
         data = np.einsum(

scripts/run_sbi_basic.py
@@ -27,6 +27,56 @@ from sbi_bmode import sim_utils, script_utils, compress_utils
 opj = os.path.join
 comm = MPI.COMM_WORLD
 
+def normalize_simple(data, data_mean, data_std):
+    '''
+    Normalize data vector.
+
+    Parameters
+    ----------
+    data : (nsim, ndata) array
+        Data vector.
+    data_mean : (ndata) array
+        Mean over simulations
+    data_std : (ndata) array
+        Standard deviation over simulations.
+
+    Returns
+    -------
+    data_norm : (nsim, ndata) array
+        Normalized data.
+    '''
+
+    shape = data.shape
+    if data.ndim == 1:
+        data = data[np.newaxis,:]
+    
+    return ((data - data_mean) / data_std).reshape(shape)
+
+def unnormalize_simple(data_norm, data_mean, data_std):
+    '''
+    Undo the normalization of a data vector.
+
+    Parameters
+    ----------
+    data_norm : (nsim, ndata) array
+        Normalized data vector.
+    data_mean : (ndata) array
+        Mean over simulations
+    data_std : (ndata) array
+        Standard deviation over simulations.
+
+    Returns
+    -------
+    data : (nsim, ndata) array
+        Unnormalized data.    
+    '''
+
+    shape = data_norm.shape
+    if data_norm.ndim == 1:
+        data_norm = data_norm[np.newaxis,:]
+        
+    return (data_norm * data_std + data_mean).reshape(shape)
+
 def get_prior(params_dict):
     '''
     Parse parameter dictionary and return pytorch prior distribution.
@@ -238,13 +288,13 @@ def main(odir, config, specdir, r_true, seed, n_train, n_samples, n_rounds, pyil
     for idx, name in enumerate(param_names):
         mean_dict[name] = float(prior.mean[idx])
 
+    norm_params = None
+    norm_simple = False
     if not pyilcdir and not no_norm:
-        norm_params = mean_dict
+        norm_params = mean_dict         
     elif pyilcdir and not no_norm:
-        norm_params = True
-    else:
-        norm_params = None
-
+        norm_simple = True
+        
     if r_true is not None:
         params_dict['r_tensor']['true_value'] = r_true
     true_params = get_true_params(params_dict)
@@ -261,6 +311,22 @@ def main(odir, config, specdir, r_true, seed, n_train, n_samples, n_rounds, pyil
 
     proposal = prior
 
+    if norm_simple:
+        # Draw some simulatons to find a normalization.
+        # Ideally this would be done during the first round of inference
+        # but easier for now to do here. We shouldn't need too many sims.
+        n_norm = 128
+        _, x_norm = simulate_for_sbi_mpi(
+            cmb_simulator, proposal, param_names, n_norm, cmb_simulator.size_data,
+            rng_sims, comm, score_compress)
+        if comm.rank == 0:
+            data_mean = np.mean(np.asarray(x_norm), axis=0)
+            data_std = np.std(np.asarray(x_norm), axis=0)        
+        else:
+            data_mean, data_std = None, None
+        data_mean = comm.bcast(data_mean, root=0)
+        data_std = comm.bcast(data_std, root=0)
+        
     # Define observations. Important that all ranks agree on this.
     if comm.rank == 0:
         x_obs = cmb_simulator.draw_data(
@@ -271,16 +337,25 @@ def main(odir, config, specdir, r_true, seed, n_train, n_samples, n_rounds, pyil
         x_obs = None
     x_obs = comm.bcast(x_obs, root=0)
 
+    if norm_simple:
+        x_obs = normalize_simple(x_obs, data_mean, data_std)
+    
     mat_compress = None
     if e_moped:
         # Draw some simulations from the prior to estimate the compression matrix.
         theta, x = simulate_for_sbi_mpi(
             cmb_simulator, proposal, param_names, n_moped, cmb_simulator.size_data,
             rng_sims, comm, score_compress)
+
         if comm.rank == 0:
+
+            if norm_simple:
+                x = normalize_simple(x.numpy(), data_mean, data_std)        
+            
             mat_compress = compress_utils.get_e_moped_matrix(x.numpy(), theta.numpy())
         mat_compress = comm.bcast(mat_compress, root=0)
         data_size = num_parameters
+        
     elif score_compress:
         data_size = num_parameters
     else:
@@ -320,9 +395,12 @@ def main(odir, config, specdir, r_true, seed, n_train, n_samples, n_rounds, pyil
 
         if comm.rank == 0:
 
+            if norm_simple:
+                x = normalize_simple(x, torch.as_tensor(data_mean), torch.as_tensor(data_std))
+            
             print(f'param draws : {theta}')
             print(f'data draws : {x}')
-
+                       
             # Save parameters and data draws to disk for debugging.
             np.save(opj(odir, f'param_draws_round_{ridx:03d}'), np.asarray(theta))
             np.save(opj(odir, f'data_draws_round_{ridx:03d}'), np.asarray(x))
@@ -347,9 +425,11 @@ def main(odir, config, specdir, r_true, seed, n_train, n_samples, n_rounds, pyil
         samples = posterior.sample((n_samples,), x=x_obs)
         np.save(opj(odir, 'samples.npy'), samples)
         if norm_params:
-            simple = True if pyilcdir is not None else False
             np.save(opj(odir, 'data_unnorm.npy'), x_obs)
-            np.save(opj(odir, 'data.npy'), cmb_simulator.get_unnorm_data(x_obs, simple=simple))
+            np.save(opj(odir, 'data.npy'), cmb_simulator.get_unnorm_data(x_obs))
+        elif norm_simple:
+            np.save(opj(odir, 'data_unnorm.npy'), x_obs)
+            np.save(opj(odir, 'data.npy'), unnormalize_simple(x_obs, data_mean, data_std))
         else:
             np.save(opj(odir, 'data.npy'), x_obs_full)
         with open(opj(odir, 'config.yaml'), "w") as handle: