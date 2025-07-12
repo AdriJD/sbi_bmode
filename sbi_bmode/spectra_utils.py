fixed bug in gradient computation of dust x sync spectrum

sbi_bmode/spectra_utils.py
@@ -209,10 +209,40 @@ def get_ell_shape(lmax, alpha, ell_pivot=80):
 
     out = jnp.zeros((lmax + 1))
     ells = jnp.arange(2, lmax + 1)
-    dells = ells * (ells + 1) / 2 / np.pi
-
+    dells = ells * (ells + 1) / 2 / jnp.pi
+    
     out = out.at[2:].set((ells / ell_pivot) ** (alpha) / dells)
+    
+    return out
 
+# I needed to use this function because just using jnp.sqrt(get_ell_shape)
+# would cause nan in the computation of the gradient. Not sure what's going
+# on, probably something similar to this:
+# https://docs.jax.dev/en/latest/faq.html#gradients-contain-nan-where-using-where
+def get_sqrt_ell_shape(lmax, alpha, ell_pivot=80):
+    '''
+    Get the square root of the ell-dependent part of the template.
+
+    Parameters
+    ----------
+    lmax : int
+        Maximum multipole.
+    alpha : float
+        Power law index.
+    ell_pivot : int, optional
+        Pivot multipole.
+
+    Returns
+    -------
+    out : (nell) array
+        Power law.
+    '''
+
+    out = jnp.zeros((lmax + 1))
+    ells = jnp.arange(2, lmax + 1)
+    dells = ells * (ells + 1) / 2 / jnp.pi
+    out = out.at[2:].set(jnp.sqrt((ells / ell_pivot) ** (alpha) / dells))
+    
     return out
 
 def bin_spectrum(spec, ells, bins, lmin, lmax, use_jax=False):
@@ -420,9 +450,10 @@ def get_dust_sync_cross_spectra(rho_ds, amp_dust, alpha_dust, amp_sync, alpha_sy
 
     out = jnp.zeros((nfreq, nfreq, lmax+1))
     out = out.at[:].set(
-        rho_ds * jnp.sqrt(amp_dust * amp_sync * get_ell_shape(lmax, alpha_dust) \
-                          * get_ell_shape(lmax, alpha_sync))[jnp.newaxis,jnp.newaxis,:])
-
+        rho_ds * jnp.sqrt(amp_dust * amp_sync) * get_sqrt_ell_shape(lmax, alpha_dust) \
+                          * get_sqrt_ell_shape(lmax, alpha_sync)[jnp.newaxis,jnp.newaxis,:])
+    
+    
     get_sed1 = lambda freq: get_sed_dust(freq, beta_dust, temp, freq_pivot_dust)
     get_sed2 = lambda freq: get_sed_sync(freq, beta_sync, freq_pivot_sync)
 

scripts/run_mcmc_basic.py
@@ -5,7 +5,6 @@ import argparse
 
 import jax
 import jax.numpy as jnp
-from jax.scipy.special import logit, expit
 import blackjax
 import matplotlib.pyplot as plt
 import numpy as np
@@ -65,6 +64,7 @@ def get_prior(params_dict):
         
     return prior_combined, param_names, transforms
 
+# ADD option for multiple data files, and optionally, true parameters of same size. DO MPI over those.
 def main(odir, config, specdir, data_file, seed, n_samples, n_chains,
          coadd_equiv_crosses=True):
     '''
@@ -97,6 +97,8 @@ def main(odir, config, specdir, data_file, seed, n_samples, n_chains,
     print(f'{params_dict=}')
     params_dict.pop('amp_beta_dust')
     params_dict.pop('gamma_beta_dust')    
+    params_dict.pop('amp_beta_sync')
+    params_dict.pop('gamma_beta_sync')    
 
     print(params_dict)
     prior_combined, param_names, transforms = get_prior(params_dict)
@@ -158,7 +160,7 @@ def main(odir, config, specdir, data_file, seed, n_samples, n_chains,
         logprob : float
             Posterior probability for input parameters.
         '''
-        
+
         model = cmb_simulator.get_signal_spectra(
             params['r_tensor'], params['A_lens'], params['A_d_BB'],
             params['alpha_d_BB'], params['beta_dust'], A_s_BB=params.get('A_s_BB'),
@@ -215,9 +217,9 @@ def main(odir, config, specdir, data_file, seed, n_samples, n_chains,
     initial_position = get_prior_draw_transformed(init_key)
     print(f'{initial_position=}')    
     print('start warmup')
-    print(logprob_transformed(initial_position))
-    print(jax.grad(logprob_transformed)(initial_position))
-
+    print(f'{logprob_transformed(initial_position)=}')
+    print(f'{jax.grad(logprob_transformed)(initial_position)=}')
+    
     warmup = blackjax.window_adaptation(
         blackjax.hmc, logprob_transformed, is_mass_matrix_diagonal=True,
         num_integration_steps=num_steps,
@@ -226,8 +228,8 @@ def main(odir, config, specdir, data_file, seed, n_samples, n_chains,
     rng_key, warmup_key, sample_key = jax.random.split(rng_key, 3)
     (state, parameters), _ = warmup.run(warmup_key, initial_position, num_steps=512)
 
-    print(state)
-    print(parameters)
+    print(f'{state=}')
+    print(f'{parameters=}')
         
     hmc = blackjax.hmc(logprob_transformed, **parameters)
     hmc_kernel = jax.jit(hmc.step)
@@ -253,31 +255,6 @@ def main(odir, config, specdir, data_file, seed, n_samples, n_chains,
 
     transform_fn = lambda position: apply_per_param_transform(position, transforms)
     transformed = jax.vmap(transform_fn)(mcmc_samples)
-
-    print('alpha_d_BB')
-    print(transformed['alpha_d_BB'])
-    print(jnp.mean(transformed['alpha_d_BB']))
-    print(jnp.std(transformed['alpha_d_BB']))
-
-    print('r_tensor')
-    print(transformed['r_tensor'])
-    print(jnp.mean(transformed['r_tensor']))
-    print(jnp.std(transformed['r_tensor']))
-
-    print('A_lens')
-    print(transformed['A_lens'])
-    print(jnp.mean(transformed['A_lens']))
-    print(jnp.std(transformed['A_lens']))
-
-    print('A_d_BB')
-    print(transformed['A_d_BB'])
-    print(jnp.mean(transformed['A_d_BB']))
-    print(jnp.std(transformed['A_d_BB']))
-
-    print('beta_dust')
-    print(transformed['beta_dust'])
-    print(jnp.mean(transformed['beta_dust']))
-    print(jnp.std(transformed['beta_dust']))
 
     # Save samples in numpy array similar to run_sbi_basic output.
     samples = np.zeros((n_samples, len(param_names)))

scripts/run_sbi_basic.py
@@ -143,7 +143,7 @@ def simulate_for_sbi_mpi(simulator, proposal, param_names, num_sims, ndata, seed
         sims_full = torch.as_tensor(sims_full.reshape(num_sims, ndata).astype(np.float32))
         if also_return_mf_data:
             sims_full_mf = torch.as_tensor(
-                sims_full_mf.reshape(num_sims, simulator.data_size_mf).astype(np.float32))
+                sims_full_mf.reshape(num_sims, simulator.size_data_mf).astype(np.float32))
 
     if also_return_mf_data:
         out = (thetas_full, sims_full, sims_full_mf)